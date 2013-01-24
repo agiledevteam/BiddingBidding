c4fc84003cdeff12a2d4153d1e4601a6e823c83b
Doojin Kang <doojin.kang@gmail.com>
2013-01-24 14:13:51 +0900

    testSingleJoinBiddingWinningLosingWiningWOn Pass

--- BiddingBiddingTest/src/com/agileteam/biddingbidding/test/BiddingBiddingEndToEndTest.java
+++ BiddingBiddingTest/src/com/agileteam/biddingbidding/test/BiddingBiddingEndToEndTest.java
@@ -44,7 +44,7 @@ public class BiddingBiddingEndToEndTest extends
 		application.showsBidding(2100, 2100);
 
 		auction.hasReceivedBid(2100, ApplicationRunner.BIDDER_ID);
-
+		
 		auction.announceClosed();
 		application.showsLost(2100, 2100);
 	}
@@ -68,6 +68,33 @@ public class BiddingBiddingEndToEndTest extends
 		application.showsWon(2100, 2200);
 	}
 
+	public void testSingleJoinBiddingWinningLosingWiningWOn() throws Exception {
+		auction.startSellingItem();
+		application.joinAuction();
+		application.showsJoined();
+		auction.hasReceivedJoinRequestFrom(ApplicationRunner.BIDDER_ID);
+
+		auction.reportPrice(2000, 100, "Other bidder");
+		application.showsLosing(2000, 2100);
+		application.bid();
+		application.showsBidding(2100, 2100);
+
+		auction.hasReceivedBid(2100, ApplicationRunner.BIDDER_ID);
+		auction.reportPrice(2100, 100, ApplicationRunner.BIDDER_ID);
+		application.showsWinning(2100, 2200);
+
+		auction.reportPrice(2200, 100, "Another bidder");
+		application.showsLosing(2200, 2300);
+
+		application.bid();
+		auction.hasReceivedBid(2300, ApplicationRunner.BIDDER_ID);
+		auction.reportPrice(2300, 100, ApplicationRunner.BIDDER_ID);
+		application.showsWinning(2300, 2400);
+
+		auction.announceClosed();
+		application.showsWon(2300, 2400);
+	}
+	
 	@Override
 	public void tearDown() throws Exception {
 		solo.finishOpenedActivities();
